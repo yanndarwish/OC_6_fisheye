bf3dd414a4a701bb42c7da2c1c61e8639d686efc
Yann Darwish <yann.darwish@gmail.com>
2022-07-18 11:07:11 +0200

    handle like on keyboard event

--- JS/templates/MediaCard.js
+++ JS/templates/MediaCard.js
@@ -26,6 +26,14 @@ class MediaCard {
             }
             likeNumberWrapper.innerHTML = media._likes
         })
+
+        // handle enter press
+        wrapper.addEventListener("keyup", e => {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+                e.target.click(); //click() sends back to the click event listenner and execute the code above
+            }
+        })
     }
     createMediaCard() {
         this._$wrapper.setAttribute('data-key', this._media.id)
@@ -43,8 +51,8 @@ class MediaCard {
                 <h4 class="media-card-name">${this._media.title}</h4>
                 <div class="media-card-like flex">
                     <p class="media-card-like-number">${this._media.likes}</p>
-                    <div class="media-card-heart" data-key="${this._media.id}">
-                        <svg class="likes" aria-label="likes" data-key="${this._media.id}"  width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <div class="media-card-heart" data-key="${this._media.id}" tabindex="0">
+                        <svg class="likes" aria-label="likes" data-key="${this._media.id}" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path data-key="${this._media.id}" d="M9.5 18.35L8.23125 17.03C3.725 12.36 0.75 9.28 0.75 5.5C0.75 2.42 2.8675 0 5.5625 0C7.085 0 8.54625 0.81 9.5 2.09C10.4537 0.81 11.915 0 13.4375 0C16.1325 0 18.25 2.42 18.25 5.5C18.25 9.28 15.275 12.36 10.7688 17.04L9.5 18.35Z" fill="#911C1C"/>
                         </svg>                                
                     </div>
